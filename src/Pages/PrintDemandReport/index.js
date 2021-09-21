35924b722880cdbf179791da14971db367217af2
Arthur <arthurarp@hotmail.com>
2021-09-21 16:38:47 -0300

    [29] Add dinamic infos (clientName and clientFeatures) to the report
    Signed-off-by: Arthur <arthurarp@hotmail.com>

--- src/Pages/PrintDemandReport/index.js
+++ src/Pages/PrintDemandReport/index.js
@@ -4,6 +4,20 @@ import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 import moment from 'moment-timezone';
 import { useProfileUser } from '../../Context';
 import { getDemandData } from '../../Services/Axios/demandsServices';
+import { getClientData, getClientFeatures } from '../../Services/Axios/clientServices';
+
+const splitFeatures = (featuresList) => {
+  let features = '';
+
+  for (let i = 0; i < featuresList.length; i += 1) {
+    if (i + 1 === featuresList.length) features += featuresList[i].name;
+    else {
+      features += featuresList[i].name;
+      features += ', ';
+    }
+  }
+  return features;
+};
 
 const DemandReport = async () => {
   pdfMake.vfs = pdfFonts.pdfMake.vfs;
@@ -11,6 +25,12 @@ const DemandReport = async () => {
   const { user, startModal } = useProfileUser();
   const date = moment.parseZone(new Date()).local(true).format('DD/MM/YYYY');
   const demandData = await getDemandData(id, startModal);
+  console.log(demandData.clientID);
+  const clientData = await getClientData(demandData.clientID, startModal)
+    .then((response) => response.data);
+  console.log(clientData);
+  const clientsFeatures = await getClientFeatures(clientData.features)
+    .then((response) => response.data);
   const document = {
     content: [
       { text: 'Divisão de Proteção à Saúde do Servidor', style: 'header' },
@@ -18,6 +38,9 @@ const DemandReport = async () => {
       { text: '\n\nRelatório de Prontuário', style: 'title' },
       { text: `\n\nUsuário: ${user.name}  Data: ${date}`, style: 'title' },
       { text: `\nDemanda: ${demandData.name}`, style: 'title' },
+      { text: `\nProcessos: ${demandData.process}`, style: 'title' },
+      { text: `\nCliente: ${clientData.name}`, style: 'title' },
+      { text: `\nCaracterísticas do Cliente: ${splitFeatures(clientsFeatures)}`, style: 'title' },
     ],
   };
   const docDefinitions = {
