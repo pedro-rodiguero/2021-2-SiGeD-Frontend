37d17e722d49133798fcefe59ae607ddf9152c08
Arthur <arthurarp@hotmail.com>
2021-09-21 15:43:12 -0300

    [29] Add initials informations to the report
    Signed-off-by: Arthur <arthurarp@hotmail.com>

--- src/Pages/PrintDemandReport/index.js
+++ src/Pages/PrintDemandReport/index.js
@@ -1,19 +1,31 @@
-/* eslint-disable */
-import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Main, H1 } from './Style';
-const DemandReport = () => {
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import moment from 'moment-timezone';
+import { useProfileUser } from '../../Context';
+import { getDemandData } from '../../Services/Axios/demandsServices';
+
+const DemandReport = async () => {
+  pdfMake.vfs = pdfFonts.pdfMake.vfs;
   const { id } = useParams();
-
-  return (
-    <Main>
-      <H1>
-        Divisão de Proteção à Saúde do Servidor
-      </H1>
-
-    </Main>
-
-  );
+  const { user, startModal } = useProfileUser();
+  const date = moment.parseZone(new Date()).local(true).format('DD/MM/YYYY');
+  const demandData = await getDemandData(id, startModal);
+  const document = {
+    content: [
+      { text: 'Divisão de Proteção à Saúde do Servidor', style: 'header' },
+      { text: 'DPSS', style: 'header' },
+      { text: '\n\nRelatório de Prontuário', style: 'title' },
+      { text: `\n\nUsuário: ${user.name}  Data: ${date}`, style: 'title' },
+      { text: `\nDemanda: ${demandData.name}`, style: 'title' },
+    ],
+  };
+  const docDefinitions = {
+    pageSize: 'A4',
+    pageMargins: [15, 50, 15, 40],
+    content: [document.content],
+  };
+  pdfMake.createPdf(docDefinitions).print();
 };
 
 export default DemandReport;
