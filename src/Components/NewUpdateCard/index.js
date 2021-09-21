9946fe0abbd3cac95cfd5f84b86b2f6c8ce17e12
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-09-21 20:43:55 -0300

    Atualizado componente NewUpdateCard form para upload de arquivos.

--- src/Components/NewUpdateCard/index.js
+++ src/Components/NewUpdateCard/index.js
@@ -4,8 +4,12 @@ import { createDemandUpdate } from '../../Services/Axios/demandsServices';
 import TinyButton from '../TinyButton';
 import {
   Card, TopSide, BottomSide, TextareaComp,
-  CheckboxContainer, CheckboxDiv,
+  CheckboxContainer, CheckboxDiv, ButtomDiv, ModalContainer, Form, ButtonsContainer,
 } from './Style';
+import {
+  Primary as AlternativeButton,
+} from '../TinyButton/Style';
+import UploadInput from '../UploadInput';
 import colors from '../../Constants/colors';
 import { useProfileUser } from '../../Context';
 
@@ -15,6 +19,8 @@ const NewUpdateCard = ({
   const [description, setDescription] = useState('');
   const [visibilityRestriction, setVisibilityRestriction] = useState(true);
   const [important, setImportant] = useState(false);
+  const [uploadFile, setUploadFile] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
   const { user, startModal } = useProfileUser();
 
   const submit = () => {
@@ -23,6 +29,37 @@ const NewUpdateCard = ({
     getDemandApi();
     setDescription('');
   };
+
+  // const uploadFileHanddler = () => {
+  //   alert('Upload file');
+  //   setOpenModal(!openModal);
+  // };
+
+  const submitForm = () => {
+    console.log('Foi');
+  };
+
+  // const [uploadFile, setUploadFile] = React.useState();
+  // const submitForm = (event) => {
+  //   event.preventDefault();
+
+  //   const dataArray = new FormData();
+  //   dataArray.append('uploadFile', uploadFile);
+
+  //   axios
+  //     .post('api_url_here', dataArray, {
+  //       headers: {
+  //         'Content-Type': 'multipart/form-data',
+  //       },
+  //     })
+  //     .then((response) => {
+  //       alert('PDF anexado com sucesso!');
+  //     })
+  //     .catch((error) => {
+  //       // error response
+  //       alert('Erro ao anexar PDF!');
+  //     });
+  // };
 
   return (
     <Card>
@@ -70,15 +107,60 @@ const NewUpdateCard = ({
             />
           </CheckboxContainer>
         </CheckboxDiv>
-        <TinyButton
-          type="primary"
-          title="Adicionar Atualização"
-          click={() => { submit(); setChangeState(!changeState); }}
-          style={{
-            width: 'max-content',
-            padding: '0% 3% 0 3%',
-          }}
-        />
+        {openModal
+          && (
+          <ModalContainer>
+            <Form onSubmit={submitForm}>
+              {console.log(uploadFile)}
+              <UploadInput accept=".pdf" fileName={uploadFile.length > 0 ? uploadFile[0].name : ''} type="file" onChange={(e) => setUploadFile(e.target.files)} />
+              <ButtonsContainer>
+                <AlternativeButton
+                  click={() => setOpenModal(false)}
+                  style={{
+                    background: `${colors.alertMessages}`,
+                    width: '100%',
+                    padding: '0% 3% 0% 3%',
+                  }}
+                >
+                  Cancelar
+                </AlternativeButton>
+
+                <AlternativeButton
+                  type="submit"
+                  style={{
+                    width: '100%',
+                    padding: '0% 3% 0% 3%',
+                  }}
+                >
+                  Enviar
+                </AlternativeButton>
+
+              </ButtonsContainer>
+
+            </Form>
+          </ModalContainer>
+          )}
+
+        <ButtomDiv>
+          <TinyButton
+            type="secundary"
+            title="Anexar PDF"
+            click={() => { setOpenModal(true); }}
+            style={{
+              width: 'max-content',
+              padding: '0% 3% 0% 3%',
+            }}
+          />
+          <TinyButton
+            type="primary"
+            title="Adicionar Atualização"
+            click={() => { submit(); setChangeState(!changeState); }}
+            style={{
+              width: 'max-content',
+              padding: '0% 3% 0 3%',
+            }}
+          />
+        </ButtomDiv>
       </BottomSide>
     </Card>
   );
