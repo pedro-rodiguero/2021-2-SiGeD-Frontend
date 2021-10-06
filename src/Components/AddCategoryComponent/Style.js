981cfed075ee9fc17676e9a634a06946f1d66928
Wictorgirardi <wictor.girardi@gmail.com>
2021-10-06 18:54:36 -0300

    separating css

--- src/Components/AddCategoryComponent/Style.js
+++ src/Components/AddCategoryComponent/Style.js
@@ -1,5 +1,5 @@
-import { IoAddCircle } from 'react-icons/io5';
-import styled from 'styled-components';
+import { IoAddCircle } from "react-icons/io5";
+import styled from "styled-components";
 
 export const AddCategory = styled.div`
   display: flex;
@@ -9,26 +9,45 @@ export const AddCategory = styled.div`
   justify-content: space-between;
   align-items: center;
 
-  @media(max-width: 750px){
+  @media (max-width: 750px) {
     padding: 8%;
   }
 `;
 
+export const styles = {
+  editIconStyle: {
+    cursor: "pointer",
+  },
+  textStyle: {
+    color: "black",
+    fontSize: "1rem",
+  },
+};
+
+export const editIconStyle = styled.div`
+  cursor: "pointer";
+`;
+
+export const textStyle = styled.div`
+  color: "black";
+  fontsize: "1rem";
+`;
+
 export const AddIcon = styled(IoAddCircle)`
-    width: 40%;
-    height: 40%;
-    color: white;
-    cursor: pointer;
-    
-    @media(max-width: 750px){
-      color: black;
-      width: auto;
-      height: auto;
-    }
+  width: 40%;
+  height: 40%;
+  color: white;
+  cursor: pointer;
+
+  @media (max-width: 750px) {
+    color: black;
+    width: auto;
+    height: auto;
+  }
 `;
 
 export const CategoriesBox = styled.div`
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   border: 2px solid black;
   border-radius: 12px;
   position: absolute;
@@ -40,7 +59,7 @@ export const CategoriesBox = styled.div`
   overflow: hidden;
   margin-right: 3%;
 
-  @media(max-width: 750px){
+  @media (max-width: 750px) {
     width: 90%;
     padding: 5px;
     overflow: auto;
@@ -55,7 +74,7 @@ export const List = styled.div`
   flex-direction: column;
   align-items: center;
 
-  @media(max-width: 750px){
+  @media (max-width: 750px) {
     height: max-content;
     padding: 5px;
     overflow: auto;
@@ -67,7 +86,7 @@ export const P = styled.p`
   margin-bottom: 0px;
   font-size: 1.5rem;
   margin-left: 10%;
-  @media(max-width: 750px){
+  @media (max-width: 750px) {
     font-size: 120%;
     color: black;
     margin-left: 3%;
@@ -75,7 +94,7 @@ export const P = styled.p`
 `;
 
 export const P2 = styled.p`
-  color: #5289B5;
+  color: #5289b5;
   margin-bottom: 0px;
   font-size: 100%;
   margin-left: 10%;
--- src/Components/AddCategoryComponent/index.js
+++ src/Components/AddCategoryComponent/index.js
@@ -1,12 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { BsPencil } from 'react-icons/bs';
+import React, { useEffect, useState } from "react";
+import { BsPencil } from "react-icons/bs";
 import {
-  AddCategory, AddIcon, CategoriesBox, List, P, CreateCategory, P2,
-} from './Style';
-import { getCategories, createCategory } from '../../Services/Axios/demandsServices';
-import CategoriesToAdd from '../CategoriesToAdd';
-import ModalComp from '../ModalComp';
-import { useProfileUser } from '../../Context';
+  AddCategory,
+  AddIcon,
+  c,
+  List,
+  P,
+  CreateCategory,
+  P2,
+  styles
+} from "./Style";
+import {
+  getCategories,
+  createCategory,
+} from "../../Services/Axios/demandsServices";
+import CategoriesToAdd from "../CategoriesToAdd";
+import ModalComp from "../ModalComp";
+import { useProfileUser } from "../../Context";
 
 const CategoryDiv = ({ pushCategory }) => {
   const [statusBox, setStatusBox] = useState(false);
@@ -14,25 +24,18 @@ const CategoryDiv = ({ pushCategory }) => {
   const [modalState, setModalState] = useState(false);
   const { startModal } = useProfileUser();
 
-  const styles = {
-    editIconStyle: {
-      cursor: 'pointer',
-    },
-    textStyle: {
-      color: 'black',
-      fontSize: '1rem',
-    },
-  };
 
   const toggleModal = () => {
     setModalState(!modalState);
   };
 
   const listCategories = async () => {
-    await getCategories('category', startModal)
+    await getCategories("category", startModal)
       .then((response) => setCategories(response.data))
       .catch((error) => {
-        console.error(`An unexpected error ocourred while getting categories.${error}`);
+        console.error(
+          `An unexpected error ocourred while getting categories.${error}`
+        );
       });
   };
 
@@ -42,9 +45,19 @@ const CategoryDiv = ({ pushCategory }) => {
 
   const renderCategories = () => {
     if (categories?.length === 0) {
-      return <p style={styles.textStyle}>Ainda não há categorias cadastradas</p>;
-    } if (categories?.length === 1) {
-      return <CategoriesToAdd category={categories[0]} key={categories[0]._id} pushCategory={pushCategory} height="50%" />;
+      return (
+        <p style={styles.textStyle}>Ainda não há categorias cadastradas</p>
+      );
+    }
+    if (categories?.length === 1) {
+      return (
+        <CategoriesToAdd
+          category={categories[0]}
+          key={categories[0]._id}
+          pushCategory={pushCategory}
+          height="50%"
+        />
+      );
     }
     return categories?.map((category) => (
       <CategoriesToAdd
@@ -60,24 +73,43 @@ const CategoryDiv = ({ pushCategory }) => {
     setStatusBox(!statusBox);
   };
   return (
-    <AddCategory onMouseLeave={() => {
-      setStatusBox(false);
-    }}
+    <AddCategory
+      onMouseLeave={() => {
+        setStatusBox(false);
+      }}
     >
       <P>Categorias:</P>
       <AddIcon onClick={toggleBox} />
-      {(statusBox && true) ? (
+      {statusBox && true ? (
         <CategoriesBox>
           <List>
             <CreateCategory>
-              <P2 onClick={toggleModal} style={styles.editIconStyle}>Criar nova categoria</P2>
-              <BsPencil color="#5289B5" onClick={toggleModal} style={styles.editIconStyle} />
+              <P2 onClick={toggleModal} style={styles.editIconStyle}>
+                Criar nova categoria
+              </P2>
+              <BsPencil
+                color="#5289B5"
+                onClick={toggleModal}
+                style={styles.editIconStyle}
+              />
             </CreateCategory>
             {renderCategories()}
           </List>
         </CategoriesBox>
       ) : null}
-      { modalState ? <ModalComp show={modalState} type="Categoria" operation="Nova " idName="" idDescription="" idColor="#000000" getContent={listCategories} handleClose={toggleModal} createContent={createCategory} /> : null}
+      {modalState ? (
+        <ModalComp
+          show={modalState}
+          type="Categoria"
+          operation="Nova "
+          idName=""
+          idDescription=""
+          idColor="#000000"
+          getContent={listCategories}
+          handleClose={toggleModal}
+          createContent={createCategory}
+        />
+      ) : null}
     </AddCategory>
   );
 };
