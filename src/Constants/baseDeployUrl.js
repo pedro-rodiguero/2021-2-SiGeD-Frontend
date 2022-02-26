4e7b361f24c10130f8916ef9211f04f3b487fbbc
murilo-dan <murilo.loiola.dan@gmail.com>
2022-02-26 16:31:26 -0300

    Testing deploy

---
+++ src/Constants/baseDeployUrl.js
@@ -0,0 +1,7 @@
+export const BaseUrlUsers = 'https://siged-users.herokuapp.com/';
+
+export const BaseUrlClients = 'https://siged-clients.herokuapp.com/';
+
+export const BaseUrlDemands = 'https://siged-demands.herokuapp.com/';
+
+export const BaseUrlSectors = 'https://siged-sectors.herokuapp.com/';
