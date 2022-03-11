cdf51060e276d9f8303114070fe6653f4f62fdf6
murilo-dan <murilo.loiola.dan@gmail.com>
2022-03-11 02:09:30 -0300

    Reorganizing workflows

--- src/Constants/baseDeployUrl.js
+++
@@ -1,7 +0,0 @@
-export const BaseUrlUsers = 'https://siged-users.herokuapp.com/';
-
-export const BaseUrlClients = 'https://siged-clients.herokuapp.com/';
-
-export const BaseUrlDemands = 'https://siged-demands.herokuapp.com/';
-
-export const BaseUrlSectors = 'https://siged-sectors.herokuapp.com/';
