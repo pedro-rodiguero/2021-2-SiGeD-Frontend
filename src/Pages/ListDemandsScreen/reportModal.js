c47d1b8677a3f21e99291acab0f6a4ba522538a4
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-19 23:02:50 -0300

    Date picker added

--- src/Pages/ListDemandsScreen/reportModal.js
+++ src/Pages/ListDemandsScreen/reportModal.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 import { FaTruckLoading } from 'react-icons/fa';
 import { useProfileUser } from '../../Context';
 import { AllDemandsReport } from '../../Utils/reports/printDemandReport';
@@ -11,9 +12,12 @@ export default function ReportModal({ allDemands, filterSector, filterCategory }
   const [currentCategory, setCurrentCategory] = useState('Todas');
   const [isGeneratingPdf, setIsGeneratingPdf] = useState(false);
   const [demandStatus, setDemandStatus] = useState('Ativas');
+  const [initialDate, setInitialDate] = useState(moment().subtract(30, 'days').format('YYYY-MM-DD'));
+  const [finalDate, setFinalDate] = useState(moment().format('YYYY-MM-DD'));
 
   useEffect(() => {
     console.log(filterCategory);
+    console.log(moment('2022-03-14T20:27:02.000Z').format('YYYY-MM-DD') >= moment('2022-03-14T20:29:02.000Z').format('YYYY-MM-DD'));
   }, []);
 
   const handleChangeSector = (e) => {
@@ -23,8 +27,17 @@ export default function ReportModal({ allDemands, filterSector, filterCategory }
   const handleChangeCategory = (e) => {
     setCurrentCategory(e.target.value);
   };
+
   const handleChangeStatus = (e) => {
     setDemandStatus(e.target.value);
+  };
+
+  const handleChangeInitialDate = (e) => {
+    setInitialDate(moment(e.target.value).format('YYYY-MM-DD'));
+  };
+
+  const handleChangeFinalDate = (e) => {
+    setFinalDate(moment(e.target.value).format('YYYY-MM-DD'));
   };
 
   const handleGeneratePdf = async () => {
@@ -69,6 +82,14 @@ export default function ReportModal({ allDemands, filterSector, filterCategory }
     setCurrentDemands(demandsFiltered);
   }, [demandStatus]);
 
+  useEffect(() => {
+    const demandsFiltered = allDemands.filter((demand) => (
+      moment(demand.createdAt).format('YYYY-MM-DD') >= moment(initialDate).format('YYYY-MM-DD')
+      && moment(demand.createdAt).format('YYYY-MM-DD') <= moment(finalDate).format('YYYY-MM-DD')
+    ));
+    setCurrentDemands(demandsFiltered);
+  }, [initialDate, finalDate]);
+
   return (
     <div>
       <h2>Selecione os filtros para gerar o relatório</h2>
@@ -111,6 +132,32 @@ export default function ReportModal({ allDemands, filterSector, filterCategory }
             ))
           }
         </select>
+      </div>
+      <div style={{
+        display: 'flex',
+        marginTop: '15px',
+        alignItems: 'center',
+      }}>
+        Data inicial:
+        <input
+          type="date"
+          id="initialDate"
+          name="initialDate"
+          value={initialDate}
+          style={{ margin: '0px 5px' }}
+          min={moment().subtract(365, 'days').format('YYYY-MM-DD')}
+          max={moment().format('YYYY-MM-DD')}
+          onChange={handleChangeInitialDate} />
+        Data final:
+        <input
+          type="date"
+          id="finalDate"
+          name="finalDate"
+          min={moment().subtract(365, 'days').format('YYYY-MM-DD')}
+          max={moment().format('YYYY-MM-DD')}
+          value={finalDate}
+          style={{ margin: '0px 5px' }}
+          onChange={handleChangeFinalDate} />
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', margin: '10px 0px' }}>
         <Button onClick={() => handleGeneratePdf()}>
