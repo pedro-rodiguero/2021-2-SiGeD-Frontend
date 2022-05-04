0ea3fa47d6c80ec6901ab9fee4d2d957141e9d2f
murilo-dan <murilo.loiola.dan@gmail.com>
2022-05-04 00:34:24 -0300

    fix client register screen bug
    Co-authored-by: Nícalo Ribeiro <nicalo63@gmail.com>

--- src/Pages/ClientRegisterScreen/index.js
+++ src/Pages/ClientRegisterScreen/index.js
@@ -1,171 +1,137 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Redirect, useHistory } from 'react-router-dom';
+import { useHistory, Redirect } from 'react-router-dom';
 import moment from 'moment';
 import GenericRegisterScreen from '../../Components/GenericRegisterScreen';
 import { validateFields } from '../../Utils/validations';
-import {
-  getClients, updateClient, getFeatures, getClientFeatures,
-} from '../../Services/Axios/clientServices';
 import ClientForms from '../../Components/ClientForms';
 import { useProfileUser } from '../../Context';
+import { postClient, getFeatures } from '../../Services/Axios/clientServices';
 
-const ClientUpdateScreen = () => {
+const ClientRegisterScreen = () => {
   const history = useHistory();
-  const [updateClientInputName, setupdateClientInputName] = useState('');
-  const [updateClientInputEmail, setupdateClientInputEmail] = useState('');
-  const [updateClientInputCpf, setupdateClientInputCpf] = useState('');
-  const [updateClientInputAddress, setupdateClientInputAddress] = useState('');
-  const [updateClientInputPhone, setupdateClientInputPhone] = useState('');
-  const [updateClientInputSecondaryPhone, setupdateClientInputSecondaryPhone] = useState('');
-  const [updateClientInputGender, setupdateClientInputGender] = useState('');
-  const [updateClientInputBirthdate, setupdateClientInputBirthdate] = useState(moment().format('DD/MM/YYYY'));
-  const [updateClientInputHealthRestrictions, setupdateClientInputHealthRestrictions] = useState('');
-  const [updateClientInputAdministrativeRestrictions, setupdateClientInputAdministrativeRestrictions] = useState('');
+  const [registerClientInputName, setRegisterClientInputName] = useState('');
+  const [registerClientInputEmail, setRegisterClientInputEmail] = useState('');
+  const [registerClientInputCpf, setRegisterClientInputCpf] = useState('');
+  const [registerClientInputAddress, setRegisterClientInputAddress] = useState('');
+  const [registerClientInputPhone, setRegisterClientInputPhone] = useState('');
+  const [registerClientInputSecondaryPhone, setregisterClientInputSecondaryPhone] = useState('');
+  const [registerClientInputGender, setRegisterClientInputGender] = useState('');
+  const [registerClientInputBirthdate, setRegisterClientInputBirthdate] = useState(moment().format('DD/MM/YYYY'));
+  const [registerClientInputHealthRestrictions, setRegisterClientInputHealthRestrictions] = useState('');
+  const [registerClientInputAdministrativeRestrictions, setRegisterClientInputAdministrativeRestrictions] = useState('');
   const [inputRegisterClientImage, setRegisterClientInputImage] = useState('');
-  const [officeOption, setOfficeOption] = useState('');
-  const [updateLocation, setupdateLocation] = useState('');
+  const [officeOption, setOfficeOption] = useState('Cargo');
+  const [registerLocation, setRegisterLocation] = useState('Lotação');
   const [featuresList, setFeaturesList] = useState([]);
-  const [clientFeaturesID, setClientFeaturesID] = useState([]);
-  const [clientFeatures, setClientFeatures] = useState([]);
+  const [selectedFeatures, setSelectedFeatures] = useState([]);
   const [selectedFeaturesID, setSelectedFeaturesID] = useState([]);
   const [baseImage, setBaseImage] = useState('');
-  const { id } = useParams();
-  const { startModal, user, token } = useProfileUser();
   const [locationName, setLocationName] = useState('');
-
-  const setExistingData = (response) => {
-    const { data } = response;
-    setupdateClientInputName(data?.name);
-    setupdateClientInputEmail(data?.email);
-    setupdateClientInputCpf(data?.cpf);
-    setupdateClientInputPhone(data?.phone);
-    setupdateClientInputSecondaryPhone(data?.secondaryPhone);
-    setupdateClientInputGender(data?.gender);
-    setupdateClientInputBirthdate(moment(data?.birthdate).format('YYYY-MM-DD'));
-    setupdateClientInputAdministrativeRestrictions(data?.administrativeRestrictions);
-    setupdateClientInputHealthRestrictions(data?.healthRestrictions);
-    setupdateClientInputAddress(data?.address);
-    setOfficeOption(data?.office);
-    if (data.location == null) {
-      data.location = 'location';
-    }
-    setLocationName(data?.location?.name || '');
-    setupdateLocation(data?.location._id);
-    setClientFeaturesID(data?.features);
-    setRegisterClientInputImage(data?.image);
-  };
-
-  const getClientFromApi = async () => {
-    getClients(`clients/${id}`, startModal)
-      .then((response) => setExistingData(response));
-  };
+  const { startModal, user } = useProfileUser();
 
   const getFeaturesFromAPI = () => {
     getFeatures('/features')
       .then((response) => setFeaturesList(response.data));
   };
 
-  const getClientFeaturesList = () => {
-    getClientFeatures(clientFeaturesID, startModal)
-      .then((response) => setClientFeatures(response.data));
-  };
-
   useEffect(() => {
-    if (token && user) {
-      getClientFromApi();
-      getFeaturesFromAPI();
-    }
-  }, [token, user]);
-
-  useEffect(() => {
-    getClientFeaturesList();
-    setSelectedFeaturesID(clientFeaturesID);
-  }, [clientFeaturesID]);
-
-  const handleChangeLocation = (value) => {
-    setLocationName(value.value.name);
-    setupdateLocation(value.id);
-  };
+    getFeaturesFromAPI();
+  }, []);
 
   const submit = async () => {
-    const phoneNoMask = updateClientInputPhone.replaceAll('(', '').replaceAll(')', '').replaceAll(' ', '').replaceAll('-', '');
-    const secondaryPhoneNoMask = updateClientInputSecondaryPhone.replaceAll('(', '').replaceAll(')', '').replaceAll(' ', '').replaceAll('-', '');
-    const validMessage = validateFields(updateClientInputName,
-      updateClientInputEmail, updateClientInputCpf,
+    const cpfNoMask = registerClientInputCpf.replaceAll('.', '').replaceAll('-', '');
+    const phoneNoMask = registerClientInputPhone.replaceAll('(', '').replaceAll(')', '').replaceAll(' ', '').replaceAll('-', '');
+    const secondaryPhoneNoMask = registerClientInputSecondaryPhone.replaceAll('(', '').replaceAll(')', '').replaceAll(' ', '').replaceAll('-', '');
+    const validMessage = validateFields(registerClientInputName,
+      registerClientInputEmail, cpfNoMask,
       phoneNoMask, secondaryPhoneNoMask);
     if (!validMessage.length) {
-      await updateClient(
-        updateClientInputName, updateClientInputEmail,
-        updateClientInputCpf, phoneNoMask,
-        secondaryPhoneNoMask, updateClientInputAddress,
-        updateClientInputGender, updateClientInputBirthdate,
-        updateClientInputAdministrativeRestrictions, updateClientInputHealthRestrictions,
-        officeOption, updateLocation, selectedFeaturesID, id, startModal, user._id,
-        baseImage,
+      const data = await postClient(
+        registerClientInputName, registerClientInputEmail,
+        cpfNoMask, phoneNoMask,
+        secondaryPhoneNoMask, registerClientInputAddress, registerClientInputGender,
+        registerClientInputBirthdate, registerClientInputHealthRestrictions,
+        registerClientInputAdministrativeRestrictions,
+        officeOption, registerLocation, selectedFeaturesID, startModal, user._id, baseImage,
       ).then((response) => response.data);
-      return history.push('/');
+      if (data) {
+        return history.push(`/perfil/${data._id}`);
+      }
+      return undefined;
     }
     startModal(validMessage.join('\n'));
     return undefined;
   };
 
   const cancel = () => {
-    history.push(`/perfil/${id}`);
+    setRegisterClientInputName('');
+    setRegisterClientInputCpf('');
+    setRegisterClientInputEmail('');
+    setRegisterClientInputPhone('');
+    setRegisterClientInputAddress('');
+    setOfficeOption('');
+    setRegisterLocation('');
+    setRegisterClientInputGender('');
+    setRegisterClientInputBirthdate('');
+    setRegisterClientInputHealthRestrictions('');
+    setRegisterClientInputAdministrativeRestrictions('');
   };
-
   if (!localStorage.getItem('@App:token')) {
     return <Redirect to="/login" />;
   }
 
+  const handleChangeLocation = (value) => {
+    setLocationName(value.value.name);
+    setRegisterLocation(value.id);
+  };
+
   return (
     <div>
-      { user && token ? (
-        <GenericRegisterScreen
-          sidebarList={[updateClientInputName, updateClientInputCpf,
-            moment(updateClientInputBirthdate).format('DD/MM/YYYY'), updateClientInputGender,
-            updateClientInputAddress, officeOption, locationName]}
-          sidebarFooter={[updateClientInputEmail, updateClientInputPhone]}
-          cancel={cancel}
-          submit={submit}
-          buttonTitle="Editar"
-          inputImage={inputRegisterClientImage}
-          setInputImage={setRegisterClientInputImage}
-          baseImage={baseImage}
-          setBaseImage={setBaseImage}
-        >
-          <ClientForms
-            setInputName={setupdateClientInputName}
-            inputName={updateClientInputName}
-            setInputEmail={setupdateClientInputEmail}
-            inputEmail={updateClientInputEmail}
-            setInputCpf={setupdateClientInputCpf}
-            inputCpf={updateClientInputCpf}
-            setInputPhone={setupdateClientInputPhone}
-            inputPhone={updateClientInputPhone}
-            setInputSecondaryPhone={setupdateClientInputSecondaryPhone}
-            secondaryPhone={updateClientInputSecondaryPhone}
-            setInputGender={setupdateClientInputGender}
-            inputGender={updateClientInputGender}
-            setInputBirthdate={setupdateClientInputBirthdate}
-            inputBirthdate={moment(updateClientInputBirthdate).format('YYYY-MM-DD')}
-            setInputAdministrativeRestriction={setupdateClientInputAdministrativeRestrictions}
-            inputAdministrativeRestriction={updateClientInputAdministrativeRestrictions}
-            setInputHealthRestrictions={setupdateClientInputHealthRestrictions}
-            inputHealthRestrictions={updateClientInputHealthRestrictions}
-            setInputAddress={setupdateClientInputAddress}
-            inputAddress={updateClientInputAddress}
-            setOfficeOption={setOfficeOption}
-            setLocationOption={handleChangeLocation}
-            locationOption={updateLocation}
-            featuresList={featuresList}
-            setSelectedFeatures={setClientFeatures}
-            selectedFeatures={clientFeatures}
-            setSelectedFeaturesID={setSelectedFeaturesID}
-          />
-        </GenericRegisterScreen>
-      ) : null}
+      <GenericRegisterScreen
+        sidebarList={[registerClientInputName, registerClientInputCpf, moment(registerClientInputBirthdate).format('DD/MM/YYYY'),
+          registerClientInputAddress, officeOption, locationName]}
+        sidebarFooter={[registerClientInputEmail, registerClientInputPhone]}
+        cancel={cancel}
+        submit={submit}
+        buttonTitle="Cadastrar"
+        inputImage={inputRegisterClientImage}
+        setInputImage={setRegisterClientInputImage}
+        baseImage={baseImage}
+        setBaseImage={setBaseImage}
+      >
+        <ClientForms
+          setInputName={setRegisterClientInputName}
+          inputName={registerClientInputName}
+          setInputEmail={setRegisterClientInputEmail}
+          inputEmail={registerClientInputEmail}
+          setInputCpf={setRegisterClientInputCpf}
+          inputCpf={registerClientInputCpf}
+          setInputGender={setRegisterClientInputGender}
+          inputGender={registerClientInputGender}
+          setInputBirthdate={setRegisterClientInputBirthdate}
+          inputBirthdate={registerClientInputBirthdate}
+          setInputAdministrativeRestriction={setRegisterClientInputAdministrativeRestrictions}
+          inputAdministrativeRestriction={registerClientInputAdministrativeRestrictions}
+          setInputHealthRestrictions={setRegisterClientInputHealthRestrictions}
+          inputHealthRestrictions={registerClientInputHealthRestrictions}
+          setInputPhone={setRegisterClientInputPhone}
+          inputPhone={registerClientInputPhone}
+          setInputSecondaryPhone={setregisterClientInputSecondaryPhone}
+          secondaryPhone={registerClientInputSecondaryPhone}
+          setInputAddress={setRegisterClientInputAddress}
+          inputAddress={registerClientInputAddress}
+          setOfficeOption={setOfficeOption}
+          setLocationOption={handleChangeLocation}
+          locationOption={registerLocation}
+          featuresList={featuresList}
+          setSelectedFeatures={setSelectedFeatures}
+          selectedFeatures={selectedFeatures}
+          setSelectedFeaturesID={setSelectedFeaturesID}
+          register
+        />
+      </GenericRegisterScreen>
     </div>
   );
 };
 
-export default ClientUpdateScreen;
+export default ClientRegisterScreen;
